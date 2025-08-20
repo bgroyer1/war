99113658523a6d4c375cc9eb95ecb5912fc8caa2
Braedan Royer <royerbraedan@gmail.com>
2025-08-19 21:47:41 -0700

    added api call function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('hello world')
+const cardApiBtn = document.querySelector('#card')
+6
+async function apiCall() {
+  try {
+    const res = await fetch('https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/')
+    if (!res.ok) {
+      throw Error(`Response failed: ${res.status}`)
+    }
+    const data = await res.json();
+    console.log(data)
+  }
+  catch(err) {
+    console.error(err)
+  }
+}
+
+cardApiBtn.addEventListener('click', async() => {
+  apiCall();
+})
